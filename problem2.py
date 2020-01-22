5f24a03b72c062cd5211a22023cd176dc2dee3e4
Matt <matthewpritzkow@yahoo.com>
2020-01-21 16:06:08 -0800

    Added both problem files

---
+++ problem2.py
@@ -0,0 +1,9 @@
+def hello(name):
+	name = input("Hello, what is your name?")
+	if name == input(name):
+		return("Hello, " + name + "!" )
+	else:
+		return("ERROR")
+
+hello("matt")
+#print('Hello, what is your name?')
